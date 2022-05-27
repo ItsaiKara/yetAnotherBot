20f8677eef1c9b186cc6de8603be94b51a1198d0
AiKa <eloytyf79@gmail.com>
2022-05-28 01:59:04 +0200

    added tags handeling on pornme (not 100% done try catch need)

--- commands/pornme.js
+++ commands/pornme.js
@@ -1,4 +1,5 @@
 const Discord= require('discord.js');
+const { blackTags } = require('./blackListTag.json');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 //var parseString = require('xml2js').parseString;
 //var DOMParser = require('dom-parser');
@@ -15,34 +16,60 @@ module.exports = {
 		.addStringOption(option =>
 		option.setName('artist')
 			.setDescription('The artist name')
-			.setRequired(true)
+			.setRequired(false)
 			.addChoices(
 				{ name: 'kairu', value: 'kairunoburogu' },
 				{ name: 'squeezable', value: 'squeezable' },
 				{ name: 'dross', value: 'dross' },
-			)),
+			))
+		.addStringOption(option =>
+		option.setName('tags')
+			.setDescription('Get images with specific tags (use the syntax "xxx+yyy+-zzz")')
+			.setRequired(false)),
 	status : "+",
 	async execute(interaction) {
-		var artist = interaction.options.get('artist').value
-		var maxPage = 0 
-		switch (maxPage) {
-		case 'kairunoburogu':
-			maxPage = 2
-			break;
-		case 'squeezable':
-			maxPage = 7
-			break;
-		case 'dross':
-			maxPage = 8
-			break;
-		default:
-			//console.log(`Sorry, we are out of ${expr}.`);
+		var artist = interaction.options.get('artist')
+		var maxPage = 1 
+		if (artist != null) {
+			switch (maxPage) {
+			case 'kairunoburogu':
+				maxPage = 2
+				break;
+			case 'squeezable':
+				maxPage = 7
+				break;
+			case 'dross':
+				maxPage = 8
+				break;
+			default:
+				maxPage = 100
+			}
+			artist = artist.value
+		}
+		if (artist == null) { 
+			if (interaction.options.get('tags') != null) {
+				maxPage = 3
+			}
+			artist = blackTags
+		}
+		if (interaction.options.get('tags') != null) {
+			//console.log(interaction.options.get('tags').value)
+			//console.log(artist)
+			var leTag = interaction.options.get('tags').value
+			//artist = artist + "+"
+			console.log("ok")
+			//artist = artist + leTag
+			console.log("ok")
+			artist = leTag + "+" + artist
+	
 		}
 		var randPage = Math.floor(Math.random() * maxPage)
-		//console.log('https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=trap+-zoophilia+-furry&limit=100&pid='+randPage)
+		//console.log(artist)
+		console.log(`https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=${artist}&limit=100&pid=${randPage}`)
 		axios
-			.get(`https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=${artist}+-zoophilia+-furry&limit=100&pid='${randPage}`)
+			.get(`https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=${artist}&limit=100&pid=${randPage}`)
 			.then(res => {
+				//console.log("ok")
 				//console.log(`statusCode: ${res.status}`);
 				//console.log(res);
 				var regex = /file_url="https:\/\/api-cdn\.rule34\.xxx\/images\/[0-9]*\/[a-z0-9]*\.[a-z0-9]*"/g
@@ -53,13 +80,15 @@ module.exports = {
 				if (interaction.channel.nsfw){
 					interaction.reply(`${img}`)
 				} else {
-
+					/*
 					interaction.reply({
 						files: [{
 							attachment: img,
 							name: "SPOILER_IMG.jpg"
 						}]
 					})
+					*/	
+					interaction.reply(`||${img}||`)			
 				}
 			})
 			.catch(error => {
