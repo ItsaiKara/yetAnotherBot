651b3118b30d6e1d415225d8897abefd8458c2b0
AiKa <eloytyf79@gmail.com>
2022-09-17 12:32:24 +0200

    reworked minecraft command

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -1,15 +1,18 @@
 const Discord= require('discord.js');
 const {SlashCommandBuilder} = require("@discordjs/builders");
+const { minecraft } = require('../config.json');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('minecraft')
-        .setDescription('Get informations about the minecraft server'),
-    status : "*",
-    async execute(interaction, mcip) {
-        if (mcip == "null"){
-            await (interaction.reply({content : "There is no minecraft server running for the USCC"}))
+        .setDescription('Get informations about the minecraft server rented if available'),
+    status : "+",
+    async execute(interaction) {
+        if(minecraft.status == "closed"){
+            interaction.reply("At the current time no server is opening anytime soon")
+        } else if(minecraft.status == "open"){
+            interaction.reply(`Current server: \n ip:${minecraft.ip} \n Server description: ${minecraft.desc}, \n`)
         } else {
-            await (interaction.reply({content: "There is a server running for the USCC, IP:** " + mcip + "**"}))
+            interaction.reply(`${minecraft.desc} \n ${minecraft.date}`)
         }
     },
 };
